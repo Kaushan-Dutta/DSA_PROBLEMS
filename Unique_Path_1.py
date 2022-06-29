27d1c9455896f9970ac50056fe80f736d05b22a5
Kaushan <kaushandutta5@gmail.com>
2022-06-29 22:01:51 +0530

    updated

--- Unique_Path_1.py
+++ Unique_Path_1.py
@@ -3,17 +3,17 @@
 
 def unique_paths(matrix,row,column):
 
-    for i in range(row):#initializing the last rows to 1
-        matrix[i][column-1]=1
-    for i in range(column):#initializing the last columns to 1
-        matrix[row-1][i]=1
+    for i in range(row):#initializing the first row to 1
+        matrix[i][0]=1
+    for i in range(column):#initializing the first column to 1
+        matrix[0][i]=1
 
-    for i in range(row-2,-1,-1):#calculating the sum of the left and right box
-        for j in range(column-2,-1,-1):
-            matrix[i][j]=matrix[i+1][j]+matrix[i][j+1]
+    for i in range(1,row):#calculating the sum of the top and left block
+        for j in range(1,column):
+            matrix[i][j]=matrix[i-1][j]+matrix[i][j-1]
 
 
-    return(matrix[0][0])#the no. of paths is the present element of the [0,0] position
+    return(matrix[row-1][column-1])#the no. of paths is the present element of the [0,0] position
 
 row,column=map(int,input().split(' '))
 matrix=[[0]*column for i in range(row)]#way to change every element of matrix to 0
