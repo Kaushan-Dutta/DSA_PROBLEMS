c46ec73c650e5be61ab8689571fe379654a1b4b6
Kaushan <kaushandutta5@gmail.com>
2022-07-11 23:22:50 +0530

    updated commit

--- BinaryAll.py
+++ BinaryAll.py
@@ -5,11 +5,16 @@ class Node():
         self.data=data
         self.left=None
         self.right=None
+class Node_linked():
+    def __init__(self,data):
+        self.data=data
+        self.next=None
 
 class Binary():
 
     def __init__(self,data):
         self.root=Node(data)
+        self.head=None
 
     def inorder(self,start,traversal):
         if(start is None):
@@ -180,7 +185,24 @@ class Binary():
 
         return dict.values()
 
+    def bottom_view(self,start):
 
+        queue=[start]
+        data=[0]
+        dict={}
+        while(len(queue)!=0):
+            hold=queue.pop(0)
+            count=data.pop(0)
+
+            dict[count]=hold.data
+
+            if (hold.left):
+                queue.append(hold.left)
+                data.append(count - 1)
+            if (hold.right):
+                queue.append(hold.right)
+                data.append(count + 1)
+        return dict.values()
 
 
     def num_nodes(self,start):
@@ -190,6 +212,8 @@ class Binary():
     def max_value(self,start):
         if (start is None): return 0
         return max(start.data,max(self.num_nodes(start.left) , self.num_nodes(start.right)))
+
+
 
 #creating the binary tree
 tree=Binary(50)
@@ -225,11 +249,12 @@ print(tree.longest_path(tree.root))#longest path from left to right in the binar
 
 print(tree.top_view(tree.root))#top view of the binary tree
 
-print(tree.bottom_view(tree.root))#bottom view of the binary tree
+print(tree.bottom_view(tree.root))#bottom view of the binary
 
 print(tree.num_nodes(tree.root))#number of nodes in the binary tree
 
 print(tree.max_value(tree.root))#max value in the binary tree
 
+#print(tree.linked_list(tree.root))
 
 #bottom view,tree from inorder preorder,tree to linked list,
